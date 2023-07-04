83c7c979314f25d1b293a5dbe85c4a063d8c15c5
ParkByeongHoon <oo009pbh@gmail.com>
2023-07-04 23:54:24 +0900

    [구조] 구조 갈아엎기 (진행중)

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
+    },
+}
+
+module.exports = nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styles
+import Main from '@pages/Main';
+import BottomTaskBar from '@template/BottomTaskBar';
+
+const Home = () => {
+  return (
+      <Main>
+        <BottomTaskBar />
+      </Main>
+  );
+};
+
+export default Home;
