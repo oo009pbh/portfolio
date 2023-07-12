95192ce01cae2dea4528a8f0937547908ca13b52
ParkByeongHoon <oo009pbh@gmail.com>
2023-07-13 01:19:50 +0900

    [구조] 구조 갈아엎기 (완료)

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ['@babel/plugin-proposal-optional-chaining', '@babel/plugin-proposal-nullish-coalescing-operator'],
+  presets: [
+    [
+      'next/babel',
+      {
+        runtime: 'automatic',
+        importSource: '@emotion/react',
+      },
+    ],
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+};
