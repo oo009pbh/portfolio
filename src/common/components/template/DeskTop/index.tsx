27a6a06f29e6db4f3c17c26c856106be51092422
ParkByeongHoon <oo009pbh@gmail.com>
2023-07-15 15:07:56 +0900

    [퍼블리싱] 바탕화면 퍼블리싱 완료

--- src/common/components/template/DeskTop/index.tsx
+++ src/common/components/template/DeskTop/index.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react';
-import useFileSystemContextState from '@hooks/useFileSystemContextState';
+// import useFileSystemContextState from '@hooks/useFileSystemContextState';
 import { DeskTopWrapper } from './styles';
 
 const ExampleComponent: React.FC = () => {
-  const { readFile } = useFileSystemContextState();
-
-  useEffect(() => {
-    const fetchFileData = async () => {
-      const data = await readFile('/my-path/my-file.txt');
-      console.log(data);
-    };
-
-    fetchFileData();
-  }, [readFile]);
+  // const { readFile } = useFileSystemContextState();
+  //
+  // useEffect(() => {
+  //   const fetchFileData = async () => {
+  //     const data = await readFile('/my-path/my-file.txt');
+  //     console.log(data);
+  //   };
+  //
+  //   fetchFileData();
+  // }, [readFile]);
 
   return <DeskTopWrapper>My Component</DeskTopWrapper>;
 };
