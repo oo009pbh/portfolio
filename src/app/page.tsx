8c36a48c9eb4e0f1c07581b6c5117c159c2db990
박병훈 <oo009pbh@gmail.com>
2024-02-14 21:56:30 +0900

    [서비스] EDIT: 메인 수정 진행중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,71 +7,73 @@ import { MainPageWrapper } from './styles';
 export default function Home() {
   return (
     <MainPageWrapper>
-      <section className="main__profile">
-        <article className="main__profile--content">
-          <div className={'main__profile--image'}>
-            <Image
-              src="/bg/bg-profile.jpg"
-              objectFit={'contain'}
-              width={300}
-              height={300}
-              alt="Picture of the Park Byeong Hoon"
-            />
+      <section className="main__container">
+        <article className="main__profile">
+          <div className="main__profile--content">
+            <div className={'main__profile--image'}>
+              <Image
+                src="/bg/bg-profile.jpg"
+                objectFit={'contain'}
+                width={300}
+                height={300}
+                alt="Picture of the Park Byeong Hoon"
+              />
+            </div>
+            <div className="main__profile--title">
+              <span>안녕하세요,</span>
+              <br />
+              <span>
+                저는 개발자 <strong>박병훈</strong> 입니다.
+              </span>
+            </div>
           </div>
-          <div className="main__profile--title">
-            <span>안녕하세요,</span>
+          <div className="main__link--container">
+            <div className="main__link--title">CONNECT WITH ME</div>
+            <div className="main__link--content">
+              <Image src="/icon/ic-mail.svg" width={32} height={32} alt="mail link" />
+              <Image src="/icon/ic-github.svg" width={32} height={32} alt="github link" />
+              <Image src="/icon/ic-linkedin.svg" width={32} height={32} alt="linkedin link" />
+              <Image src="/icon/ic-velog.svg" width={32} height={32} alt="velog link" />
+            </div>
+          </div>
+        </article>
+        <article className="main__summary">
+          <div className="about__tag">ABOUT ME</div>
+          <div className="about__title">
+            사용하기 편한 컴포넌트
             <br />
-            <span>
-              저는 개발자 <strong>박병훈</strong> 입니다.
-            </span>
+            개발을 지향합니다.
           </div>
-        </article>
-        <div className="main__link--container">
-          <div className="main__link--title">CONNECT WITH ME</div>
-          <div className="main__link--content">
-            <Image src="/icon/ic-mail.svg" width={32} height={32} alt="mail link" />
-            <Image src="/icon/ic-github.svg" width={32} height={32} alt="github link" />
-            <Image src="/icon/ic-linkedin.svg" width={32} height={32} alt="linkedin link" />
-            <Image src="/icon/ic-velog.svg" width={32} height={32} alt="velog link" />
+          <div className="about__description">
+            Si 스타트업에서 2년 6개월째 프론트엔드 개발을 맡고 있으며, <br />
+            읽기 쉬운 코드로 금방 사용법을 알 수 있는 컴포넌트를 개발 하는 것을 좋아합니다. 기획자, 디자이너, 타 개발자
+            분들과 소통하며 전체 개발 일정을 조율하고 프로젝트를 출시하는 것에 큰 보람을 느낍니다.
           </div>
-        </div>
-      </section>
-      <section className="main__summary">
-        <div className="about__tag">ABOUT ME</div>
-        <article className="about__title">
-          사용하기 편한 컴포넌트
-          <br />
-          개발을 지향합니다.
-        </article>
-        <article className="about__description">
-          Si 스타트업에서 2년 6개월째 프론트엔드 개발을 맡고 있으며, <br />
-          읽기 쉬운 코드로 금방 사용법을 알 수 있는 컴포넌트를 개발 하는 것을 좋아합니다. 기획자, 디자이너, 타 개발자
-          분들과 소통하며 전체 개발 일정을 조율하고 프로젝트를 출시하는 것에 큰 보람을 느낍니다.
-        </article>
-        <article className="about__info">
-          <div className="about__info--category">Education</div>
-          <div className="about__info--container">
-            <div className="about__info--title">세종대학교</div>
-            <div className="about__info--content">2016.02 ~ 2022.03</div>
+          <div className="about__info">
+            <div className="about__info--category">Education</div>
+            <div className="about__info--container">
+              <div className="about__info--title">세종대학교</div>
+              <div className="about__info--content">2016.02 ~ 2022.03</div>
+            </div>
+            <div className="about__info--container">
+              <div className="about__info--title">전공</div>
+              <div className="about__info--content">소프트웨어 심화</div>
+            </div>
+            <div className="about__info--container">
+              <div className="about__info--title">전공학점</div>
+              <div className="about__info--content">3.7 / 4.5</div>
+            </div>
           </div>
-          <div className="about__info--container">
-            <div className="about__info--title">전공</div>
-            <div className="about__info--content">소프트웨어 심화</div>
-          </div>
-          <div className="about__info--container">
-            <div className="about__info--title">전공학점</div>
-            <div className="about__info--content">3.7 / 4.5</div>
-          </div>
-        </article>
-        <article className="about__info">
-          <div className="about__info--category">Etc</div>
-          <div className="about__info--container">
-            <div className="about__info--title">SQLD</div>
-            <div className="about__info--content">2022.12</div>
-          </div>
-          <div className="about__info--container">
-            <div className="about__info--title">해군전역</div>
-            <div className="about__info--content">2018.02 ~ 2019.12</div>
+          <div className="about__info">
+            <div className="about__info--category">Etc</div>
+            <div className="about__info--container">
+              <div className="about__info--title">SQLD</div>
+              <div className="about__info--content">2022.12</div>
+            </div>
+            <div className="about__info--container">
+              <div className="about__info--title">해군전역</div>
+              <div className="about__info--content">2018.02 ~ 2019.12</div>
+            </div>
           </div>
         </article>
       </section>
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -3,93 +3,98 @@ import styled from 'styled-components';
 import { flexCssGenerator, fontGenerator } from '@styles';
 
 export const MainPageWrapper = styled.main`
-  ${flexCssGenerator('flex', 'center', 'flex-start')};
   width: 100%;
   height: 100%;
   padding: 8rem 5rem;
   position: relative;
 
-  & > section.main__profile {
-    width: 55rem;
+  & > section.main__container {
+    width: 100%;
     height: 100%;
-    ${flexCssGenerator('flex', 'space-between', 'flex-start', 'column')};
+    ${flexCssGenerator('flex', 'center', 'flex-start')};
 
-    & > article.main__profile--content {
-      & > div.main__profile--image {
-        width: 30rem;
-        height: 30rem;
-        margin-bottom: 3rem;
-      }
+    & > article.main__profile {
+      width: 55rem;
+      height: 100%;
+      ${flexCssGenerator('flex', 'space-between', 'flex-start', 'column')};
 
-      & > div.main__profile--title {
-        width: 45rem;
-        height: 8rem;
-
-        & span {
-          color: var(--black-02);
-          ${fontGenerator('3.5rem', '400', '4rem')}
+      & > div.main__profile--content {
+        & > div.main__profile--image {
+          width: 30rem;
+          height: 30rem;
+          margin-bottom: 3rem;
         }
 
-        & strong {
-          color: var(--black-02);
-          ${fontGenerator('3.5rem', '500', '4rem')}
+        & > div.main__profile--title {
+          width: 45rem;
+          height: 8rem;
+
+          & span {
+            color: var(--black-02);
+            ${fontGenerator('3.5rem', '400', '4rem')}
+          }
+
+          & strong {
+            color: var(--black-02);
+            ${fontGenerator('3.5rem', '500', '4rem')}
+          }
+        }
+      }
+
+      & > div.main__link--container {
+        & > div.main__link--title {
+          ${fontGenerator('1.6rem', '400', '2.2rem')}
+
+          margin-bottom: 1.5rem;
+        }
+
+        & > div.main__link--content {
+          ${flexCssGenerator('flex', 'flex-start', 'center')};
+          gap: 1.3rem;
         }
       }
     }
 
-    & > div.main__link--container {
-      & > div.main__link--title {
-        ${fontGenerator('1.6rem', '400', '2.2rem')}
+    & > article.main__summary {
+      width: 45rem;
 
-        margin-bottom: 1.5rem;
-      }
-
-      & > div.main__link--content {
-        ${flexCssGenerator('flex', 'flex-start', 'center')};
-        gap: 1.3rem;
-      }
-    }
-  }
-
-  & > section.main__summary {
-    width: 45rem;
-
-    & > div.about__tag {
-      ${fontGenerator('1.4rem', '400', '2.2rem')}
-      color: var(--blue-01);
-      margin-bottom: 1rem;
-    }
-
-    & > article.about__title {
-      ${fontGenerator('2.8rem', '700', '3.2rem')}
-      margin-bottom: 1.4rem;
-      height: 6.4rem;
-    }
-
-    & > article.about__description {
-      margin-bottom: 3.5rem;
-    }
-
-    & > article.about__info {
-      & > div.about__info--category {
-        ${fontGenerator('1.4rem', '700', '2rem')}
-        margin-bottom: 1.4rem;
-      }
-
-      & > div.about__info--container {
-        ${flexCssGenerator('flex', 'space-between', 'center', 'flex-start')};
+      & > div.about__tag {
+        ${fontGenerator('1.4rem', '400', '2.2rem')}
+        color: var(--blue-01);
         margin-bottom: 1rem;
+      }
 
-        & > div.about__info--title {
-          color: var(--gray-80);
+      & > div.about__title {
+        ${fontGenerator('2.8rem', '700', '3.2rem')}
+        margin-bottom: 1.4rem;
+        height: 6.4rem;
+      }
+
+      & > div.about__description {
+        margin-bottom: 3.5rem;
+      }
+
+      & > div.about__info {
+        & > div.about__info--category {
+          ${fontGenerator('1.4rem', '700', '2rem')}
+          margin-bottom: 1.4rem;
         }
 
-        & > div.about__info--content {
-          color: var(--gray-60);
-        }
+        & > div.about__info--container {
+          ${flexCssGenerator('flex', 'space-between', 'center', 'flex-start')};
+          margin-bottom: 1rem;
 
-        &:nth-last-of-type(1) {
-          margin-bottom: 2.4rem;
+          & > div.about__info--title {
+            color: var(--gray-80);
+          }
+
+          & > div.about__info--content {
+            color: var(--gray-60);
+          }
+
+          &:nth-last-of-type(1) {
+            margin-bottom: 2.4rem;
+          }
         }
       }
     }
